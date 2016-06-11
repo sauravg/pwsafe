31afe8247c0007b3cb500a8361b29b1bc26f8227
Saurav Ghosh <saurav@mbp>
2016-06-11 11:22:05 +0530

    Use wcout for wide char strings

--- src/ui/cli/main.cpp
+++ src/ui/cli/main.cpp
@@ -529,7 +529,7 @@ StringX GetNewPassphrase()
 StringX GetPassphrase(const wstring& prompt)
 {
     wstring wpk;
-    cout << prompt;
+    wcout << prompt;
     echoOff();
     wcin >> wpk;
     echoOn();
@@ -583,7 +583,7 @@ std::vector<Restriction> ParseSearchedEntryRestrictions(const wstring &restrictT
 int OpenCore(PWScore& core, const StringX& safe)
 {
   if (!pws_os::FileExists(safe.c_str())) {
-    cerr << safe << " - file not found" << endl;
+    wcerr << safe << " - file not found" << endl;
     return 2;
   }
 
@@ -731,7 +731,7 @@ int SearchForEntries(PWScore &core, const wstring &searchText, bool ignoreCase,
 
   ::FindMatches(std2stringx(searchText), ignoreCase, fields, restrictions.size() > 0, r.value, r.field, r.rule, r.caseSensitive,
               core.GetEntryIter(), core.GetEntryEndIter(), get_second<ItemList>{}, [](ItemListIter itr){
-                cout << itr->second.GetGroup() << " - " << itr->second.GetTitle() << " - " << itr->second.GetUser() << endl;
+                wcout << itr->second.GetGroup() << " - " << itr->second.GetTitle() << " - " << itr->second.GetUser() << endl;
   });
   return PWScore::SUCCESS;
 }
