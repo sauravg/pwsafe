3c72a59775eb9abfd931fe9d49658a8aa8d534b6
saurav <sauravg@users.sf.net>
2016-09-12 17:54:40 +0530

    Force the core to be saved if successfully created
    New safes are not marked as dirty. We have to save it anyhow.

--- src/ui/cli/cli_create_safe.h
+++ src/ui/cli/cli_create_safe.h
@@ -21,12 +21,23 @@ public:
   }
   virtual int execute(PWScore &core, const string &op_param)              override;
   friend int open_core<cli_create_safe>(PWScore &core, const StringX &safe);
+  friend int save_core<cli_create_safe>(PWScore &core, const cli_create_safe &t);
 };
 
 template <>
 inline int open_core<cli_create_safe>(PWScore &core, const StringX &safe) {
   core.SetCurFile(safe);
   return PWScore::SUCCESS;
+}
+
+// A new safe is not marked as "changed", so we override this
+// here to ensure a new safe is alway saved, unless it was a dry run
+template <>
+inline int save_core<cli_create_safe>(PWScore &core, const cli_create_safe &t)
+{
+  const cli_task& base = t;
+  return !t.dry_run? SaveCore(core, base.dry_run):
+              PWScore::SUCCESS;
 }
 
 #endif
